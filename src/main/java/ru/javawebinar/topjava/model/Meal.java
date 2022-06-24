bc94fffc2c59fd2ccf507533de928a5aa344ab7f
Petr Pichugin <petr.pichugin@gmail.com>
2022-06-24 14:10:53 +0500

    HW04 base (JpaMealRepository & tests)

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,18 +1,52 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE,
+                query = "DELETE FROM Meal m " +
+                        "WHERE m.id=:id AND m.user.id=:userId"),
+        @NamedQuery(name = Meal.ALL_SORTED,
+                query = "SELECT m FROM Meal m " +
+                        "WHERE m.user.id=:userId " +
+                        "ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.HALF_OPEN,
+                query = "SELECT m FROM Meal m " +
+                        "WHERE m.user.id=:userId AND m.dateTime >=:startDateTime AND m.dateTime <:endDateTime " +
+                        "ORDER BY m.dateTime DESC"),
+})
+@Entity
+@Table(name = "meals", uniqueConstraints = {
+        @UniqueConstraint(name = "meals_unique_user_datetime_idx", columnNames = {"user_id", "date_time"})})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "Meal.delete";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String HALF_OPEN = "Meal.getBetweenHalfOpen";
+
+    @Column(name = "date_time", nullable = false, unique = true)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(min = 3, max = 200)
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @Range(min = 10, max = 10000)
     private int calories;
 
+    @JoinColumn(name = "user_id", nullable = false)
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
